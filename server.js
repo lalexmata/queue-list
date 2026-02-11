2151821070c189e7f6f787a2757e7b85d529fc95
Alexander Mata <puchomata17@gmail.com>
2026-02-11 14:47:55 -0300

    fix: host railway

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const { createApp } = require("./src/app");
 
 const PORT = process.env.PORT || 5005;
+const HOST = "0.0.0.0"; // ✅ IMPORTANTE en Railway 
 
 const app = createApp();
-app.listen(PORT, () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
