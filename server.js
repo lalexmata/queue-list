1da65280ab180ca30b9d7dfb3c443c9ca3a24174
Alexander Mata <puchomata17@gmail.com>
2026-02-11 17:56:07 -0300

    feat: prueba 2 server

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 require("dotenv").config({ quiet: true });
-const { createApp } = require("./src/app");
+
+process.on("uncaughtException", (e) => console.error("UNCAUGHT", e));
+process.on("unhandledRejection", (e) => console.error("UNHANDLED", e));
+
+let createApp;
+try {
+  ({ createApp } = require("./src/app"));
+} catch (e) {
+  console.error("BOOT ERROR requiring ./src/app:", e);
+  process.exit(1);
+}
 
 const PORT = Number(process.env.PORT || 8080);
 
-const app = createApp();
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+let app;
+try {
+  app = createApp();
+} catch (e) {
+  console.error("BOOT ERROR creating app:", e);
+  process.exit(1);
+}
+
+app.listen(PORT, () => console.log("Listening on", PORT));
