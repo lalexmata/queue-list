c697d1a154d731fe9c29496332624dc10f983711
Alexander Mata <puchomata17@gmail.com>
2026-02-11 17:53:37 -0300

    fix: test server

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
 require("dotenv").config({ quiet: true });
-
-console.log("ENV CHECK:", {
-  PORT: process.env.PORT,
-  HAS_DATABASE_URL: !!process.env.DATABASE_URL,
-  HAS_BROADCASTER_LOGIN: !!process.env.BROADCASTER_LOGIN,
-});
-
-process.on("uncaughtException", (e) => {
-  console.error("UNCAUGHT EXCEPTION:", e);
-});
-process.on("unhandledRejection", (e) => {
-  console.error("UNHANDLED REJECTION:", e);
-});
-
 const { createApp } = require("./src/app");
 
-const PORT = process.env.PORT || 8080;
-const HOST = "0.0.0.0";
+const PORT = Number(process.env.PORT || 8080);
 
 const app = createApp();
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
