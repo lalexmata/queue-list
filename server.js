f7f4efb5ac4f5121200ec25a81b51d71beea0a67
Alexander Mata <puchomata17@gmail.com>
2026-02-11 15:03:09 -0300

    feat: log en server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config({quiet: true});
 const { createApp } = require("./src/app");
 
+process.on("unhandledRejection", (err) => console.error("❌ UnhandledRejection:", err));
+process.on("uncaughtException", (err) => console.error("❌ UncaughtException:", err));
+
 const PORT = Number(process.env.PORT || 5005);
 const HOST = "0.0.0.0"; // ✅ IMPORTANTE en Railway 
 
